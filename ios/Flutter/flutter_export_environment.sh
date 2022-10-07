250ad1d53e13b685b61422e089625dd8742bc6d7
mafdaniels <mafabidaniel1@gmail.com>
2022-10-07 12:44:50 +0300

    my first mobile app commit

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/Santos/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/Santos/AndroidStudioProjects/uber_clone_with_flutter"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_ROOT=E:\Prgrams\mak\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Hp\Desktop\cloned flutter\Flutter-Uber-clone"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/Santos/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
